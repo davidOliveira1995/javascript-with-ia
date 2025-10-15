b818df48df9e96707543a153b3b1ad2955a9c05c
DavidOliveira <srnowdkzinx22@gmail.com>
2025-10-15 20:44:10 -0300

    first commit

---
+++ exe01.js
@@ -0,0 +1,15 @@
+const myName = "David";
+const myAge = 30;
+
+console.log(`My name is ${myName} and I have ${myAge} years old.`);
+
+let a = "10";
+let b = 5;
+
+console.log(Number(a) + b);
+
+
+const language = "Portuguese";
+
+language = "English";
+
---
+++ exe02.js
@@ -0,0 +1,20 @@
+const x = 10;
+const y = 3;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x ** y);
+
+let num = 5;
+
+num += 3;
+console.log(num);
+
+num *= 2;
+console.log(num);
+
+console.log("5" == 5); // Verifica se os dois valores são iguais. True.
+console.log("5" === 5); // Verifica se os dois valores são iguais e do mesmo tipo em JavaScript False.
